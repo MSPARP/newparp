9e3e638ddee3532a2f2036dafcf0831295974ffc
Sho <qsjjtran@gmail.com>
2014-03-08 12:45:29 -0500

    adding class

--- charat2/static/js/chat.js
+++ charat2/static/js/chat.js
@@ -174,7 +174,7 @@ function addLine(msg){
     }
 
     var mp = $('<p>').attr("id","message"+msg.id).addClass(msg.type).addClass("user"+msg.user_id).css('color', '#'+msg.color).html(alias+message).appendTo(CONVERSATION_ID);
-    var mi = $('<p>').addClass("message"+msg.id).addClass("info").html('<span class="left">'+user_list[msg.user_id]+'</span><span class="right">'+Date(msg.posted)+'</span>').appendTo(CONVERSATION_ID);
+    var mi = $('<p>').addClass("message"+msg.id).addClass("info").addClass("user"+msg.user_id).html('<span class="left">'+user_list[msg.user_id]+'</span><span class="right">'+Date(msg.posted)+'</span>').appendTo(CONVERSATION_ID);
 
     if (at_bottom) {
         goBottom(CONVERSATION_CONTAINER);
