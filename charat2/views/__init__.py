0fe8e5fdc2e90cccc440ef579439f966306de9dd
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-03-24 17:04:01 +0000

    Listed rooms.

--- charat2/views/__init__.py
+++ charat2/views/__init__.py
@@ -1,5 +1,6 @@
 from flask import g, render_template
 
+from charat2.model import GroupChat
 from charat2.model.connections import use_db
 from charat2.helpers.auth import login_required
 
@@ -13,7 +14,8 @@ def home():
 @use_db
 @login_required
 def rooms():
-    if g.user is not None:
-        return render_template("rooms.html")
-    else:
-        abort(404)
+    rooms_query = g.db.query(GroupChat).filter(GroupChat.publicity=="listed")
+    rooms = [(_, g.redis.scard("chat:%s:online" % _.id)) for _ in rooms_query]
+    rooms.sort(key=lambda _: _[1], reverse=True)
+    return render_template("rooms.html", rooms=rooms)
+
