d1d13a84ca0e390c686ffb90ba24e374ccdc73e6
Sho <qsjjtran@gmail.com>
2014-03-29 11:26:22 -0400

    test the fix in user_buttons

--- charat2/static/js/chat.js
+++ charat2/static/js/chat.js
@@ -664,7 +664,7 @@ $(document).ready(function() {
         
         /* User Action Settings */
         $('.user_buttons .set li').on('click',function(){
-            setGroup($(this).attr('class'),$(this).parent('li').attr('class'));
+            setGroup($(this).parent('li').parent().parent().attr('class'),$(this).attr('class'));
             console.log($(this).attr('class'));
             console.log($(this).parent('li').attr('class'));
         });
