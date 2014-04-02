69723b1c3c6576c97787dadb23073780427958a2
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-04-02 17:14:20 +0100

    RP subdomain.

--- charat2/__init__.py
+++ charat2/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from charat2.model.connections import (
@@ -10,6 +12,7 @@ from charat2.model.connections import (
 from charat2.views import home, rooms, account, chat, chat_api
 
 app = Flask(__name__)
+app.config["SERVER_NAME"] = os.environ["BASE_DOMAIN"]
 
 app.before_request(redis_connect)
 
@@ -19,25 +22,29 @@ app.after_request(db_commit)
 app.teardown_request(db_disconnect)
 app.teardown_request(redis_disconnect)
 
-app.add_url_rule("/", "home", home, methods=("GET",))
+# Root domain (charat.net)
 
-app.add_url_rule("/rooms", "rooms", rooms, methods=("GET",))
+app.add_url_rule("/", "home", home, methods=("GET",))
 
 app.add_url_rule("/login", "log_in", account.log_in, methods=("POST",))
 app.add_url_rule("/logout", "log_out", account.log_out, methods=("POST",))
 app.add_url_rule("/register", "register", account.register, methods=("POST",))
 
-app.add_url_rule("/create_chat", "create_chat", chat.create_chat, methods=("POST",))
-app.add_url_rule("/chat/<url>", "chat", chat.chat, methods=("GET",))
+# RP subdomain (rp.charat.net)
 
-app.add_url_rule("/chat_api/messages", "messages", chat_api.messages, methods=("POST",))
-app.add_url_rule("/chat_api/send", "send", chat_api.send, methods=("post",))
-app.add_url_rule("/chat_api/set_state", "set_state", chat_api.set_state, methods=("POST",))
-app.add_url_rule("/chat_api/set_group", "set_group", chat_api.set_group, methods=("POST",))
-app.add_url_rule("/chat_api/user_action", "user_action", chat_api.user_action, methods=("POST",))
-app.add_url_rule("/chat_api/set_flag", "set_flag", chat_api.set_flag, methods=("POST",))
-app.add_url_rule("/chat_api/set_topic", "set_topic", chat_api.set_topic, methods=("POST",))
-app.add_url_rule("/chat_api/save", "save", chat_api.save, methods=("POST",))
-app.add_url_rule("/chat_api/ping", "ping", chat_api.ping, methods=("POST",))
-app.add_url_rule("/chat_api/quit", "quit", chat_api.quit, methods=("POST",))
+app.add_url_rule("/rooms", "rooms", rooms, subdomain="rp", methods=("GET",))
+
+app.add_url_rule("/create_chat", "create_chat", chat.create_chat, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat/<url>", "chat", chat.chat, subdomain="rp", methods=("GET",))
+
+app.add_url_rule("/chat_api/messages", "messages", chat_api.messages, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/send", "send", chat_api.send, subdomain="rp", methods=("post",))
+app.add_url_rule("/chat_api/set_state", "set_state", chat_api.set_state, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/set_group", "set_group", chat_api.set_group, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/user_action", "user_action", chat_api.user_action, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/set_flag", "set_flag", chat_api.set_flag, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/set_topic", "set_topic", chat_api.set_topic, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/save", "save", chat_api.save, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/ping", "ping", chat_api.ping, subdomain="rp", methods=("POST",))
+app.add_url_rule("/chat_api/quit", "quit", chat_api.quit, subdomain="rp", methods=("POST",))
 
