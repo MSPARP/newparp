2788635c49b8a92199cfc6f9754fee4b1b18b48f
Sho Tran <qsjjtran@gmail.com>
2014-04-05 19:42:14 -0400

    login page

--- charat2/views/rp/__init__.py
+++ charat2/views/rp/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import g, render_template, request
+from flask import g, render_template, request, redirect, url_for
 
 from charat2.model import GroupChat
 from charat2.model.connections import use_db
