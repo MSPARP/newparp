c22b3020ddc284fb5867a47b0627f89f41e34706
Sho Tran <qsjjtran@gmail.com>
2014-04-05 02:22:48 -0400

    fixing error messages

--- charat2/views/root.py
+++ charat2/views/root.py
@@ -4,6 +4,6 @@ from charat2.model.connections import use_db
 
 @use_db
 def home():
-    logged_in = True if g.user is None else False
+    logged_in = True if g.user is not None else False
     return render_template("home.html", logged_in=logged_in)
 
