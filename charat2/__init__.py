25dfb85c9b27269414f8ff21b028bf95c77e4526
Sho Tran <qsjjtran@gmail.com>
2014-04-19 16:24:22 -0400

    flask babel tryi

--- charat2/__init__.py
+++ charat2/__init__.py
@@ -20,6 +20,7 @@ app.url_map.strict_slashes = False
 app.config["SERVER_NAME"] = os.environ["BASE_DOMAIN"]
 
 babel = Babel(app)
+app.jinja_env.globals.update(format_datetime=format_datetime)
 
 app.before_request(redis_connect)
 
