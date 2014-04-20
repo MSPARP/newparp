b9ffac1ebda573d2d6e6ba4122bdbbf3e8bcde86
Sho Tran <qsjjtran@gmail.com>
2014-04-20 03:16:41 -0400

    timestamp 24hr time fix

--- charat2/static/js/chat.js
+++ charat2/static/js/chat.js
@@ -96,7 +96,7 @@ if (typeof String.prototype.endsWith != 'function') {
 function getTimestamp(seconds_from_epoch) {
     var month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var timestamp = new Date(seconds_from_epoch*1000);
-    return month_names[timestamp.getMonth()]+' '+timestamp.getDate()+' '+(timestamp.getHours()<10?'0':'')+timestamp.getHours()+':'+(timestamp.getMinutes()<10?'0':'')+timestamp.getMinutes();
+    return month_names[timestamp.getMonth()]+' '+timestamp.getDate()+' '+(timestamp.getHours()==0?'0':'')+timestamp.getHours()+':'+(timestamp.getMinutes()<10?'0':'')+timestamp.getMinutes();
 }
 
 function topbarSelect(selector) {
