3154fb9cc967767356e3ccb48954fae78b11b843
Sho Tran <qsjjtran@gmail.com>
2014-04-22 02:32:38 -0400

    recaptcha?

--- charat2/__init__.py
+++ charat2/__init__.py
@@ -15,7 +15,6 @@ from charat2.views.rp import chat, chat_api
 from flask.ext.babel import Babel, gettext
 
 from flask_wtf import Form, RecaptchaField
-from wtforms import TextField
 
 RECAPTCHA_PRIVATE_KEY = '6LetOPISAAAAAKN2LCPUYt7sk1Ogh7rWXVGEWZ9Z'
 RECAPTCHA_PUBLIC_KEY = '6LetOPISAAAAAEBLq4_tCYcr1XcA3LO3rrQX2Th9'
