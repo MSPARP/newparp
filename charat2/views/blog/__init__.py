2eb68053d0232c80dce19ef2149a71ad11470a70
Sho Tran <qsjjtran@gmail.com>
2014-04-24 12:40:32 -0400

    blog feed

--- charat2/views/blog/__init__.py
+++ charat2/views/blog/__init__.py
@@ -4,13 +4,4 @@ from charat2.model.connections import use_db
 
 @use_db
 def feed():
-    json = [{
-        "title" : "Test Blog",
-        "content" : "lorem ipsum testing everything"
-    },
-    {
-        "title" : "Test Bloggy Stuff",
-        "content" : "lorem ipsum testing everything yyyyeee"
-    }]
-    #return jsonify(**json)
     return "ok"
