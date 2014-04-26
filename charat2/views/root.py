f49416c1e201167d300ce3ec72784dd4bf4cae45
Sho Tran <qsjjtran@gmail.com>
2014-04-25 21:15:10 -0400

    blog stylings

--- charat2/views/root.py
+++ charat2/views/root.py
@@ -13,14 +13,17 @@ def home():
 
 @use_db
 def feed():
-    data = []
-    for i in range(1,50):
-        post = {
-            "id" : i,
-            "title" : "Test Blog "+i,
-            "author" : "Sho Tran",
-            "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }
-        data.append(post)
+    data = [{
+        "id" : 1,
+        "title" : "Test Blog",
+        "author" : "Sho Tran",
+        "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        "id" : 2,
+        "title" : "Test Bloggy Stuff",
+        "author" : "Sho Tran",
+        "content" : "lorem ipsum testing everything yyyyeee"
+    }]
     return json.dumps(data, sort_keys=True)
 
