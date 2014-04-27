60edcfed7b3e91f6324c76f5ec7fc5b96c71a175
Sho Tran <qsjjtran@gmail.com>
2014-04-27 19:59:40 -0400

    reorganize js

--- charat2/static/js/desktop-notify.js
+++ charat2/static/js/desktop-notify.js
@@ -194,3 +194,37 @@
         Object.seal(win.notify);
     }
 }(window));
+
+function NotificationCenter($scope) {
+    var permissionLevels = {};
+    permissionLevels[notify.PERMISSION_GRANTED] = 0;
+    permissionLevels[notify.PERMISSION_DEFAULT] = 1;
+    permissionLevels[notify.PERMISSION_DENIED] = 2;
+
+    $scope.isSupported = notify.isSupported;
+    $scope.permissionLevel = permissionLevels[notify.permissionLevel()];
+
+    $scope.getClassName = function() {
+        if ($scope.permissionLevel === 0) {
+            return "allowed"
+        } else if ($scope.permissionLevel === 1) {
+            return "default"
+        } else {
+            return "denied"
+        }
+    }
+
+    $scope.callback = function() {
+        console.log("test");
+    }
+
+    $scope.requestPermissions = function() {
+        notify.requestPermission(function() {
+            $scope.$apply($scope.permissionLevel = permissionLevels[notify.permissionLevel()]);
+        })
+    }
+}
+
+function show(title,body) {
+    notify.createNotification(title, {body:body, icon: "{{url_for("static", filename="img/notification.png")}}", tag:title+body+Math.random(), timeout: 5000})
+}
