c0982c0b5c44684bc8fc7954c781b9eecc86df20
Sho Tran <qsjjtran@gmail.com>
2014-04-28 12:19:01 -0400

    blog making

--- charat2/views/root.py
+++ charat2/views/root.py
@@ -3,11 +3,11 @@ import json
 from flask import g, render_template, request, redirect, url_for
 
 from charat2.model.connections import use_db
-from charat2 import blog
+#from charat2.blog import feed
 
 @use_db
 def home():
-    posts = json.loads(blog.feed())
+    posts = json.loads(feed())
     return render_template(
         "root/home.html",
         logged_in=g.user is not None,
