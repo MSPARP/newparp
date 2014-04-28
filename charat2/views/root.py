648c3023fcff931b95cc5cbe66bcd2034cca6a92
Sho Tran <qsjjtran@gmail.com>
2014-04-28 12:17:24 -0400

    blog making

--- charat2/views/root.py
+++ charat2/views/root.py
@@ -1,14 +1,13 @@
 import json
-from urllib import urlopen
 
 from flask import g, render_template, request, redirect, url_for
 
 from charat2.model.connections import use_db
+from charat2 import blog
 
 @use_db
 def home():
-    jsonurl = urlopen(url_for("blog_feed"))
-    posts = json.loads(jsonurl.read())
+    posts = json.loads(blog.feed())
     return render_template(
         "root/home.html",
         logged_in=g.user is not None,
