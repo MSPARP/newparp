4505598f421f497c96afd55654111cdb865af203
Sho Tran <qsjjtran@gmail.com>
2014-05-03 17:24:29 -0400

    blog post enhance

--- charat2/__init__.py
+++ charat2/__init__.py
@@ -71,5 +71,6 @@ app.add_url_rule("/chat_api/quit", "quit", chat_api.quit, subdomain="rp", method
 # Blog subdomain (blog.charat.net)
 
 app.add_url_rule("/", "blog_home", blog.home, subdomain="blog",methods=("GET",))
+app.add_url_rule("/post/<id>", "blog_post", blog.post, subdomain="blog",methods=("GET",))
 app.add_url_rule("/feed.json", "blog_feed", blog.feed, subdomain="blog",methods=("GET",))
 
--- charat2/views/blog/__init__.py
+++ charat2/views/blog/__init__.py
@@ -14,53 +14,40 @@ def home():
     )
 
 @use_db
+def post(id):
+    posts = json.loads(feed())
+    post = posts[id]
+    return render_template(
+        "blog/home.html",
+        logged_in=g.user is not None,
+        post=post
+    )
+
+@use_db
 def feed():
-    data = [{
+    data = {
+    1 : {
         "id" : 1,
         "title" : "Test Blog",
         "author" : "Sho Tran",
         "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
-    {
+    2 : {
         "id" : 2,
         "title" : "Test Bloggy Stuff",
         "author" : "Sho Tran",
         "content" : "lorem ipsum testing everything yyyyeee"
     },
-    {
+    3 : {
         "id" : 3,
         "title" : "Test Bloggy Stuff",
         "author" : "Sho Tran",
         "content" : "lorem ipsum testing everything yyyyeee"
     },
-    {
+    4 : {
         "id" : 4,
         "title" : "Test Bloggy Stuff",
         "author" : "Sho Tran",
         "content" : "lorem ipsum testing everything yyyyeee"
-    },
-    {
-        "id" : 5,
-        "title" : "Test Bloggy Stuff",
-        "author" : "Sho Tran",
-        "content" : "lorem ipsum testing everything yyyyeee"
-    },
-    {
-        "id" : 6,
-        "title" : "Test Bloggy Stuff",
-        "author" : "Sho Tran",
-        "content" : "lorem ipsum testing everything yyyyeee"
-    },
-    {
-        "id" : 7,
-        "title" : "Test Bloggy Stuff",
-        "author" : "Sho Tran",
-        "content" : "lorem ipsum testing everything yyyyeee"
-    },
-    {
-        "id" : 8,
-        "title" : "Test Bloggy Stuff",
-        "author" : "Sho Tran",
-        "content" : "lorem ipsum testing everything yyyyeee"
-    }]
+    }}
     return json.dumps(data, sort_keys=True)
