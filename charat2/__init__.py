d45f4ee66a9798400bcbf3e0ae7e7c0f700be8d6
Sho Tran <qsjjtran@gmail.com>
2014-05-03 17:49:22 -0400

    blog post enhance

--- charat2/__init__.py
+++ charat2/__init__.py
@@ -71,6 +71,6 @@ app.add_url_rule("/chat_api/quit", "quit", chat_api.quit, subdomain="rp", method
 # Blog subdomain (blog.charat.net)
 
 app.add_url_rule("/", "blog_home", blog.home, subdomain="blog",methods=("GET",))
-app.add_url_rule("/post/<int:post_id>", "blog_post", blog.view_post, subdomain="blog",methods=("GET",))
+app.add_url_rule("/post/<post_id>", "blog_post", blog.view_post, subdomain="blog",methods=("GET",))
 app.add_url_rule("/feed.json", "blog_feed", blog.feed, subdomain="blog",methods=("GET",))
 
