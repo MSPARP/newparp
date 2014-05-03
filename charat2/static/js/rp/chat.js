3b76360c6b03641963da331aa2244b0de10435c9
Sho Tran <qsjjtran@gmail.com>
2014-05-03 03:40:51 -0400

    rp fixes

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -516,6 +516,7 @@ $(document).ready(function() {
                     $('#control-buttons .ooc-button').css('background-color','');
                 }
             } else {
+                console.log("test2");
                 type_force = 'me';
                 $('#control-buttons .me-button').css('background-color','#70A070');
                 $('#control-buttons .ooc-button').css('background-color','');
