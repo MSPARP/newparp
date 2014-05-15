5532c3a50923f213b32332b2695ec009ab4f81f3
Sho Tran <qsjjtran@gmail.com>
2014-05-15 13:29:39 -0400

    rp fixes

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -492,8 +492,14 @@ function updateChatPreview(){
     }
     
     if (command[0] == '/ban') {
-        var action_user = user_list[command[1]];
-        textPreview = "banned [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] ";
+        try {
+            var action_user = user_list[command[1]];
+            textPreview = "banned [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] ";
+        } except {
+            aliasPreview = "";
+            textPreview = "[color=#EE0000]Error[/color]";
+        }
+        
     }
     
     if (command[0] == '/kick') {
