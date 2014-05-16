d13a1f198663d674ca945864e1996c7e04d63ae6
Sho Tran <qsjjtran@gmail.com>
2014-05-16 02:31:52 -0400

    rp fixes

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -517,10 +517,11 @@ function updateChatPreview(){
         var groups = ['magical','cute','little','unsilence','silence'];
         var group_map = {'magical':'mod', 'cute':'mod2', 'little':'mod3','unsilent':'user','silence':'silent'};
         try {
-            if (groups.contains(command[2]))
+            if (groups.indexOf(command[2]) > 0) {
                 var action_user = user_list[command[1]];
                 var group_set = GROUP_DESCRIPTIONS[group_map[command[2]]];
-                textPreview = "set [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] to "+group_set.title+(group_set.description ? "They can now "+group_set.description : "");
+                textPreview = "set [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] to "+group_set.title+"."+(group_set.description ? " They can now "+group_set.description : "");
+            }
         } catch(e) {
             aliasPreview = "";
             textPreview = "[color=#EE0000]Error[/color]";
