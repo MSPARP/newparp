b168120407dd079630fb6bbd37f820c04e34cdbc
Sho Tran <qsjjtran@gmail.com>
2014-05-16 02:34:18 -0400

    rp fixes

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -516,7 +516,7 @@ function updateChatPreview(){
             if (groups.indexOf(command[2]) > 0) {
                 var action_user = user_list[command[1]];
                 var group_set = GROUP_DESCRIPTIONS[group_map[command[2]]];
-                textPreview = "set [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] to "+group_set.title+"."+(group_set.description ? " They can now "+group_set.description : "");
+                textPreview = "set [color=#"+action_user.character.color+"]"+action_user.character.name+"[/color] [[color=#"+action_user.character.color+"]"+action_user.character.acronym+"[/color]] to "+group_set.title+"."+(group_set.description ? " They can now "+group_set.description+"." : "");
             }
         } catch(e) {
             aliasPreview = "";
