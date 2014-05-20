7ffbf6135401333c596261b3f8f81930b8c7e725
Sho <qsjjtran@gmail.com>
2014-05-20 17:50:32 -0400

    aliasOffset

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -463,8 +463,8 @@ function readCookie(name) {
     return null;
 }
 
-function updateChatPreview(){
-    $('#aliasOffset').css('top', (5-$('#textInput').scrollTop())+px);
+function updateChatPreview() {
+    $('#aliasOffset').css('top', (5-$('#textInput').scrollTop())+'px');
     var at_bottom = atBottom(CONVERSATION_CONTAINER);
     var textPreview = $('#textInput').val().replace(/\r\n|\r|\n/g,"[br]");
     $('#textInput').css('opacity','1');
