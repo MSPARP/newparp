2707e178f107b76a6790f76974dcf344b72c80cc
Sho Tran <qsjjtran@gmail.com>
2014-05-22 10:56:13 -0700

    topic button fixing

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -243,8 +243,8 @@ function generateUserList(user_data) {
         var user_description = GROUP_DESCRIPTIONS[list_user.meta.group].title+(GROUP_DESCRIPTIONS[list_user.meta.group].description ? ' – '+GROUP_DESCRIPTIONS[list_user.meta.group].description : '')
         if (list_user.meta.user_id == user.meta.user_id) {
             is_self = " self";
-            $(USER_LIST_ID).prop('class',list_user.meta.group);
-            $("#userList").prop('class',list_user.meta.group);
+            $(USER_LIST_ID).prop('class', list_user.meta.group);
+            $("#userList").prop('class', "sidebar "+list_user.meta.group);
         }
         
         if ($('#user'+list_user.meta.user_id).length <= 0) {
