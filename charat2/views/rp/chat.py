564c26e4d1b3a5e2e9d1cc8d31d34640c3d106ff
Sho Tran <qsjjtran@gmail.com>
2014-05-22 02:11:39 -0400

    PM fixes

--- charat2/views/rp/chat.py
+++ charat2/views/rp/chat.py
@@ -86,7 +86,7 @@ def chat(url):
             chat = PMChat(url=pm_url)
             g.db.add(chat)
             g.db.flush()
-        chat.title = "Private Messaging "+pm_user
+        chat['title'] = "Private Messaging "+pm_user
 
     else:
 
