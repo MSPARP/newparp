1cead450f688fe0a5cc69cc247042d7161ef41a9
Sho Tran <qsjjtran@gmail.com>
2014-05-23 22:01:17 -0400

    login_required referer

--- charat2/helpers/auth.py
+++ charat2/helpers/auth.py
@@ -1,11 +1,11 @@
-from flask import abort, g, redirect, url_for
+from flask import abort, g, redirect, request, url_for
 from functools import wraps
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.user is None:
-            return redirect(url_for('login_get'))
+            return redirect(url_for('login_get')+"?referer="+request.path)
         return f(*args, **kwargs)
     return decorated_function
 
