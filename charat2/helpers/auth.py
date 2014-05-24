f207cd2d4694c593df3c82556603cdaaeaccf937
Sho Tran <qsjjtran@gmail.com>
2014-05-23 22:03:00 -0400

    login_required referer

--- charat2/helpers/auth.py
+++ charat2/helpers/auth.py
@@ -5,7 +5,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.user is None:
-            return redirect(url_for('login_get')+"?referer="+request.path)
+            return redirect(url_for('login_get')+"?referer="+request.url)
         return f(*args, **kwargs)
     return decorated_function
 
