933b30c114e7808c9d966109506a9ea56a5d15cd
Sho Tran <qsjjtran@gmail.com>
2014-05-24 22:41:04 -0400

    uninternal server error

--- charat2/views/rp/chat.py
+++ charat2/views/rp/chat.py
@@ -208,7 +208,7 @@ def log(url, page=None):
     ).scalar()
     
     if page is None:
-        page = message_count/100
+        page = 1
     
     messages = g.db.query(Message).filter(
         Message.chat_id==chat.id,
