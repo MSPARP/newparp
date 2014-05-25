a22f591c5e8132a2f13d9ccb622626d3073fb92a
Sho Tran <qsjjtran@gmail.com>
2014-05-25 15:50:25 -0400

    uninternal server error

--- charat2/helpers/auth.py
+++ charat2/helpers/auth.py
@@ -5,7 +5,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.user is None:
-            return abort(403)
+            return redirect(url_for('login_get')+"?log_in_error=You need to be logged in to access this page.&referer="+request.url)
         return f(*args, **kwargs)
     return decorated_function
 
