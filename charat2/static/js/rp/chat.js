bfd58c149f601e8081fd54308fc98ab9c6f2ebe9
Sho Tran <qsjjtran@gmail.com>
2014-05-27 23:25:35 -0700

    desktop notifications on only me ic and ooc

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -231,7 +231,7 @@ function addLine(msg){
         }
 
         if (!document.hasFocus() && desktop_notifications == true) {
-            if (msg.type == 'ic' || msg.type = 'ooc' || msg.type == 'me') {
+            if (msg.type == 'ic' || msg.type == 'ooc' || msg.type == 'me') {
                 desktopNotification(chat.title,alias+bbRemoveAll(msg.text),'http://charat.thae.li/static/img/favicons/rp/touch-icon-ipad.png');
             }
         }
