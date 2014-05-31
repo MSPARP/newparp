11103591e35f57196a8b1917cd680fdcc10b19a9
Sho Tran <qsjjtran@gmail.com>
2014-05-30 23:04:17 -0400

    online testing

--- charat2/views/rp/chat_list.py
+++ charat2/views/rp/chat_list.py
@@ -110,6 +110,7 @@ def chat_list(fmt=None, type=None, page=1):
         type=type,
         chats=chat_dicts,
         paginator=paginator,
-        chat_classes=chat_classes
+        chat_classes=chat_classes,
+        test=g.redis.scard("chat:%s:online" % chat['id']))
     )
 
