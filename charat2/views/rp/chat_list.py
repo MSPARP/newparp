98726a7ea01cd13573bff2e19f5c21348f266206
nepeat <nepeat@users.noreply.github.com>
2014-05-30 18:26:52 -0800

    (chats list) debug

--- charat2/views/rp/chat_list.py
+++ charat2/views/rp/chat_list.py
@@ -88,7 +88,7 @@ def chat_list(fmt=None, type=None, page=1):
         cd["unread"] = c[1].last_message > c[0].last_online
         chat_dicts.append(cd)
 
-    chat_dicts = [(chat, g.redis.scard("chat:%s:online" % chat['id'])) for chat in chats]
+    chat_dicts = [(chat, g.redis.scard("chat:%s:online" % chat['id'])) for chat in chat_dicts]
 
     if fmt == "json":
 
