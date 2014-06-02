5228bfdea0f79f343d675406bdd6b83641de44b7
Sho Tran <qsjjtran@gmail.com>
2014-06-02 10:15:23 -0700

    chats.js

--- charat2/static/js/rp/chats.js
+++ charat2/static/js/rp/chats.js
@@ -11,59 +11,59 @@ function chatsUpdate(first) {
     
     if (current_mode == mode || first) {
         if ($('body.mobile').length>0) {
-            $('</div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
             for(chat in chats) {
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-1');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-1');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-1');
                 }
             }
         } else if ($('body.nobile').length>0) {
-            $('</div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
-            $('</div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
             for (var i=0; i<chats.length; i+=2) {
                 chat = chats[i];
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-1');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-1');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-1');
                 }
             }
             for (var i=1; i<chats.length; i+=2) {
                 chat = chats[i];
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-2');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-2');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-2');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-2');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-2');
                 }
             }
         } else {
-            $('</div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
-            $('</div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
-            $('</div>').addClass('section').prop('id', 'column-3').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-3').appendTo('#under-page');
             for (var i=0; i<chats.length; i+=3) {
                 chat = chats[i];
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-1');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-1');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-1');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-1');
                 }
             }
             for (var i=1; i<chats.length; i+=3) {
                 chat = chats[i];
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-2');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-2');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-2');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-2');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-2');
                 }
             }
             for (var i=2; i<chats.length; i+=3) {
                 chat = chats[i];
-                $('</div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-3');
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url).appendTo('#column-3');
                 $('</a>').addClass('title').prop('href','/'+chat.url).text(chat.url+(chat.unread ? ' '+chat.url : '')).append('#column-3');
                 if (chat.type == 'group') {
-                    $('</div>').addClass('topic').text(chat.topic).appendTo('#column-3');
+                    $('<div>').addClass('topic').text(chat.topic).appendTo('#column-3');
                 }
             }
         }
