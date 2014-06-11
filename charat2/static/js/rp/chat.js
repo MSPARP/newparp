6d565e8f74c139651645747a13a8a28a98be5c00
Namastane <qsjjtran@gmail.com>
2014-06-11 16:17:16 -0700

    chatList testing

--- charat2/static/js/rp/chat.js
+++ charat2/static/js/rp/chat.js
@@ -1054,6 +1054,7 @@ $(function(){
                     $('<div>').prop('id', chatData.id).addClass('card selection').appendTo('#chatPick .list');
                     $('<h1>').addClass('titi').text(chatData.title).appendTo('#'+chatData.id);
                     $('#'+chatData.id).on('click', function() {
+                        console.log(chatData);
                         addChatData(chatData);
                     });
                 }
