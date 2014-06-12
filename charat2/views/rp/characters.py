f929cda90bb6e27bef2a2ba8bb11b8977d0d50f3
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-06-12 02:26:57 +0100

    Creating new characters.

--- charat2/views/rp/characters.py
+++ charat2/views/rp/characters.py
@@ -1,4 +1,4 @@
-from flask import g, render_template
+from flask import g, redirect, render_template, url_for
 
 from charat2.helpers.auth import login_required
 from charat2.model import UserCharacter
@@ -14,4 +14,19 @@ def character_list():
             UserCharacter.user_id==g.user.id,
         ).order_by(UserCharacter.title).all(),
     )
+
+
+@use_db
+@login_required
+def new_character():
+    character = UserCharacter(user_id=g.user.id, title="Untitled character")
+    g.db.add(character)
+    g.db.flush()
+    return redirect(url_for("character", character_id=character.id))
+
+
+@use_db
+@login_required
+def character(character_id):
+    raise NotImplementedError
 
