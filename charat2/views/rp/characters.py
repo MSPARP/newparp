5cd3c656709a09056f1c3dce4204df220dd58a4f
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-06-14 01:28:28 +0100

    Saving user characters.

--- charat2/views/rp/characters.py
+++ charat2/views/rp/characters.py
@@ -1,4 +1,6 @@
-from flask import abort, g, jsonify, redirect, render_template, url_for
+import json
+
+from flask import abort, g, jsonify, redirect, render_template, request, url_for
 from sqlalchemy import and_
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -6,6 +8,18 @@ from charat2.helpers import alt_formats
 from charat2.helpers.auth import login_required
 from charat2.model import case_options, UserCharacter
 from charat2.model.connections import use_db
+from charat2.model.validators import color_validator
+
+
+def user_character_query(character_id):
+    try:
+        return g.db.query(UserCharacter).filter(and_(
+            UserCharacter.id == character_id,
+            UserCharacter.user_id == g.user.id,
+        )).order_by(UserCharacter.title).one()
+    except NoResultFound:
+        abort(404)
+
 
 @alt_formats(set(["json"]))
 @use_db
@@ -39,13 +53,7 @@ def new_character():
 @login_required
 def character(character_id, fmt=None):
 
-    try:
-        character = g.db.query(UserCharacter).filter(and_(
-            UserCharacter.id == character_id,
-            UserCharacter.user_id == g.user.id,
-        )).order_by(UserCharacter.title).one()
-    except NoResultFound:
-        abort(404)
+    character = user_character_query(character_id)
 
     if fmt == "json":
         return jsonify(character.to_dict(include_options=True))
@@ -61,40 +69,71 @@ def character(character_id, fmt=None):
 @login_required
 def save_character(character_id):
 
-    try:
-        character = g.db.query(UserCharacter).filter(and_(
-            UserCharacter.id == character_id,
-            UserCharacter.user_id == g.user.id,
-        )).order_by(UserCharacter.title).one()
-    except NoResultFound:
-        abort(404)
+    character = user_character_query(character_id)
 
-    raise NotImplementedError
+    # yeah this is just cut and pasted from chat_api.py
+
+    # Don't allow a blank title.
+    if request.form["title"] == "":
+        abort(400)
+
+    # Don't allow a blank name.
+    if request.form["name"] == "":
+        abort(400)
+
+    # Validate color.
+    if not color_validator.match(request.form["color"]):
+        abort(400)
+    character.color = request.form["color"]
+
+    # Validate case.
+    if request.form["case"] not in case_options:
+        abort(400)
+    character.case = request.form["case"]
+
+    # There are length limits on the front end so just silently truncate these.
+    character.title = request.form["title"][:50]
+    character.name = request.form["name"][:50]
+    character.acronym = request.form["acronym"][:15]
+    character.quirk_prefix = request.form["quirk_prefix"][:50]
+    character.quirk_suffix = request.form["quirk_suffix"][:50]
+
+    # XXX PUT LENGTH LIMIT ON REPLACEMENTS?
+    # Zip replacements.
+    replacements = zip(
+        request.form.getlist("quirk_from"),
+        request.form.getlist("quirk_to"),
+    )
+    # Strip out any rows where from is blank or the same as to.
+    replacements = [_ for _ in replacements if _[0] != "" and _[0] != _[1]]
+    # And encode as JSON.
+    character.replacements = json.dumps(replacements)
+
+    # XXX PUT LENGTH LIMIT ON REGEXES?
+    # Zip regexes.
+    regexes = zip(
+        request.form.getlist("regex_from"),
+        request.form.getlist("regex_to"),
+    )
+    # Strip out any rows where from is blank or the same as to.
+    regexes = [_ for _ in regexes if _[0] != "" and _[0] != _[1]]
+    # And encode as JSON.
+    character.regexes = json.dumps(regexes)
+
+    return redirect(url_for("character", character_id=character.id))
 
 
 @use_db
 @login_required
 def delete_character_get(character_id):
-    try:
-        character = g.db.query(UserCharacter).filter(and_(
-            UserCharacter.id == character_id,
-            UserCharacter.user_id == g.user.id,
-        )).order_by(UserCharacter.title).one()
-    except NoResultFound:
-        abort(404)
+    character = user_character_query(character_id)
     return render_template("rp/delete_character.html", character_id=character_id)
 
 
 @use_db
 @login_required
 def delete_character_post(character_id):
-    try:
-        character = g.db.query(UserCharacter).filter(and_(
-            UserCharacter.id == character_id,
-            UserCharacter.user_id == g.user.id,
-        )).order_by(UserCharacter.title).one()
-    except NoResultFound:
-        abort(404)
+    character = user_character_query(character_id)
     g.db.delete(character)
     return redirect(url_for("character_list"))
 
