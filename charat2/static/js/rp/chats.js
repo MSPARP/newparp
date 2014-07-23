b746c516a09004057cab0549651270cb08d0e728
Sho Tran <qsjjtran@gmail.com>
2014-07-22 18:37:04 -0700

    chats.js test

--- charat2/static/js/rp/chats.js
+++ charat2/static/js/rp/chats.js
@@ -28,91 +28,90 @@ function chatsUpdate(first) {
             unread_chats++;
         }
     }
+
     if (unread_chats > 0) {
         document.title = unread_chats+" unread - "+ORIGINAL_TITLE;
     } else {
         document.title = ORIGINAL_TITLE;
     }
-    console.log("test");
     if (current_mode == mode || first) {
-        console.log(mode);
         $('#under-page').empty();
-        //if ($('body.mobile').length>0) {
-        //    $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
-        //    for(i in chats) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //} else if ($('body.nobile').length>0) {
-        //    $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
-        //    $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
-        //    for (var i=0; i<chats.length; i+=2) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //    for (var i=1; i<chats.length; i+=2) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-2');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //} else {
-        //    $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
-        //    $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
-        //    $('<div>').addClass('section').prop('id', 'column-3').appendTo('#under-page');
-        //    for (var i=0; i<chats.length; i+=3) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //    for (var i=1; i<chats.length; i+=3) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-2');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //    for (var i=2; i<chats.length; i+=3) {
-        //        chat = chats[i];
-        //        $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-3');
-        //        $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        if (chat.online > 0) {
-        //            $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
-        //        }
-        //        if (chat.type == 'group') {
-        //            $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
-        //        }
-        //    }
-        //}
+        if ($('body.mobile').length>0) {
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            for(i in chats) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+        } else if ($('body.nobile').length>0) {
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
+            for (var i=0; i<chats.length; i+=2) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+            for (var i=1; i<chats.length; i+=2) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-2');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+        } else {
+            $('<div>').addClass('section').prop('id', 'column-1').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-2').appendTo('#under-page');
+            $('<div>').addClass('section').prop('id', 'column-3').appendTo('#under-page');
+            for (var i=0; i<chats.length; i+=3) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-1');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+            for (var i=1; i<chats.length; i+=3) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-2');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+            for (var i=2; i<chats.length; i+=3) {
+                chat = chats[i];
+                $('<div>').addClass('card chat').prop('id', 'chat-'+chat.url.replace(/\//g,'-')).appendTo('#column-3');
+                $('<a>').addClass('title').prop('href','/'+chat.url).text(chat.title+(chat.unread ? ' (unread)' : '')).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                if (chat.online > 0) {
+                    $('<div>').addClass('users-online').text(chat.online+' online').appendTo('#chat-'+chat.url.replace(/\//g,'-').replace(/\//g,'-'));
+                }
+                if (chat.type == 'group') {
+                    $('<div>').addClass('topic').html(bbEncode(chat.topic)).appendTo('#chat-'+chat.url.replace(/\//g,'-'));
+                }
+            }
+        }
     }
     
     if ($('#column-1').is(':empty')) {
