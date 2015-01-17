5b71bf1fe8ac1c2625573c6f8a2122ef386a73e6
Olly Parton <mysticdragonfly@hotmail.co.uk>
2015-01-17 23:41:07 +0000

    Move most of the matchmaker code to helpers. #7
    This will allow us to repeat a lot less stuff in the roulette matchmaker.

---
+++ charat2/helpers/matchmaker.py
@@ -0,0 +1,101 @@
+import time
+import json
+import logging
+
+from random import shuffle
+from redis import StrictRedis
+from uuid import uuid4
+
+from charat2.model import sm, ChatUser, Message
+from charat2.model.connections import redis_pool
+
+option_messages = {
+    "script": "This is a script style chat.",
+    "paragraph": "This is a paragraph style chat.",
+    "sfw": "Please keep this chat safe for work.",
+    "nsfw": "NSFW content is allowed.",
+}
+
+
+def run_matchmaker(get_searcher_info, check_compatibility, ChatClass, get_character_info):
+
+    # XXX get log level from stdin
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    db = sm()
+    redis = StrictRedis(connection_pool=redis_pool)
+
+    searcher_ids = redis.smembers("searchers")
+
+    while True:
+
+        # Reset the searcher list for the next iteration.
+        redis.delete("searchers")
+        for searcher in searcher_ids:
+            logging.debug("Waking unmatched searcher %s." % searcher)
+            redis.publish("searcher:%s" % searcher, "{ \"status\": \"unmatched\" }")
+
+        time.sleep(10)
+
+        logging.info("Starting match loop.")
+
+        searcher_ids = redis.smembers("searchers")
+
+        # We can't do anything with less than 2 people, so don't bother.
+        if len(searcher_ids) < 2:
+            logging.info("Not enough searchers, skipping.")
+            continue
+
+        searchers = get_searcher_info(redis, searcher_ids)
+        logging.debug("Searcher list: %s" % searchers)
+        shuffle(searchers)
+
+        already_matched = set()
+        # Range hack so we don't check opposite pairs or against itself.
+        for n in range(len(searchers)):
+            s1 = searchers[n]
+
+            for m in range(n + 1, len(searchers)):
+                s2 = searchers[m]
+
+                if s1["id"] in already_matched or s2["id"] in already_matched:
+                    continue
+
+                logging.debug("Comparing %s and %s." % (s1["id"], s2["id"]))
+
+                match, options = check_compatibility(s1, s2)
+                if not match:
+                    logging.debug("No match.")
+                    continue
+
+                new_url = str(uuid4()).replace("-", "")
+                logging.info(
+                    "Matched %s and %s, sending to %s."
+                    % (s1["id"], s2["id"], new_url)
+                )
+                new_chat = ChatClass(url=new_url)
+                db.add(new_chat)
+                db.flush()
+
+                db.add(ChatUser(chat_id=new_chat.id, user_id=s1["user_id"], number=1, **get_character_info(db, s1)))
+                db.add(ChatUser(chat_id=new_chat.id, user_id=s2["user_id"], number=2, **get_character_info(db, s2)))
+
+                if options:
+                    db.add(Message(
+                        chat_id=new_chat.id,
+                        type="search_info",
+                        text=" ".join(option_messages[_] for _ in options),
+                    ))
+
+                db.commit()
+
+                already_matched.add(s1["id"])
+                already_matched.add(s2["id"])
+
+                match_message = json.dumps({ "status": "matched", "url": new_url })
+                redis.publish("searcher:%s" % s1["id"], match_message)
+                redis.publish("searcher:%s" % s2["id"], match_message)
+                searcher_ids.remove(s1["id"])
+                searcher_ids.remove(s2["id"])
+
--- charat2/workers/matchmaker.py
+++ charat2/workers/matchmaker.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python
 
-import time
-import json
-import logging
-
-from random import shuffle
-from redis import StrictRedis
-from sqlalchemy import and_
-from sqlalchemy.orm import joinedload
-from sqlalchemy.orm.exc import NoResultFound
-from uuid import uuid4
-
-from charat2.helpers.chat import send_message
-from charat2.model import sm, ChatUser, Message, SearchedChat, User
-from charat2.model.connections import redis_pool
+from charat2.helpers.matchmaker import run_matchmaker
+from charat2.model import SearchedChat
 
 
-option_messages = {
-    "script": "This is a script style chat.",
-    "paragraph": "This is a paragraph style chat.",
-    "sfw": "Please keep this chat safe for work.",
-    "nsfw": "NSFW content is allowed.",
-}
+def get_searcher_info(redis, searcher_ids):
+    searchers = []
+    for searcher_id in searcher_ids:
+        session_id = redis.get("searcher:%s:session_id" % searcher_id)
+        # This will fail they've logged out since sending the request.
+        try:
+            user_id = int(redis.get("session:%s" % session_id))
+            search_character_id = int(redis.get("searcher:%s:search_character_id" % searcher_id))
+        except (TypeError, ValueError):
+            continue
+        searchers.append({
+            "id": searcher_id,
+            "user_id": user_id,
+            "search_character_id": search_character_id,
+            "character": redis.hgetall("searcher:%s:character" % searcher_id),
+            "options": redis.hgetall("searcher:%s:options" % searcher_id),
+            "choices": {int(_) for _ in redis.smembers("searcher:%s:choices" % searcher_id)},
+        })
+    return searchers
 
 
 def check_compatibility(s1, s2):
@@ -61,101 +62,10 @@ def check_compatibility(s1, s2):
     return False, None
 
 
+def get_character_info(db, searcher):
+    return searcher["character"]
+
+
 if __name__ == "__main__":
-
-    # XXX get log level from stdin
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-
-    db = sm()
-    redis = StrictRedis(connection_pool=redis_pool)
-
-    searcher_ids = redis.smembers("searchers")
-
-    while True:
-
-        # Reset the searcher list for the next iteration.
-        redis.delete("searchers")
-        for searcher in searcher_ids:
-            logging.debug("Waking unmatched searcher %s." % searcher)
-            redis.publish("searcher:%s" % searcher, "{ \"status\": \"unmatched\" }")
-
-        time.sleep(10)
-
-        logging.info("Starting match loop.")
-
-        searcher_ids = redis.smembers("searchers")
-
-        # We can't do anything with less than 2 people, so don't bother.
-        if len(searcher_ids) < 2:
-            logging.info("Not enough searchers, skipping.")
-            continue
-
-        searchers = []
-        for searcher in searcher_ids:
-            session_id = redis.get("searcher:%s:session_id" % searcher)
-            # Don't match them if they've logged out since sending the request.
-            try:
-                user_id = int(redis.get("session:%s" % session_id))
-                search_character_id = int(redis.get("searcher:%s:search_character_id" % searcher))
-            except (TypeError, ValueError):
-                continue
-            searchers.append({
-                "id": searcher,
-                "user_id": user_id,
-                "search_character_id": search_character_id,
-                "character": redis.hgetall("searcher:%s:character" % searcher),
-                "options": redis.hgetall("searcher:%s:options" % searcher),
-                "choices": {int(_) for _ in redis.smembers("searcher:%s:choices" % searcher)},
-            })
-        logging.debug("Searcher list: %s" % searchers)
-        shuffle(searchers)
-
-        already_matched = set()
-        # Range hack so we don't check opposite pairs or against itself.
-        for n in range(len(searchers)):
-            s1 = searchers[n]
-
-            for m in range(n + 1, len(searchers)):
-                s2 = searchers[m]
-
-                if s1["id"] in already_matched or s2["id"] in already_matched:
-                    continue
-
-                logging.debug("Comparing %s and %s." % (s1["id"], s2["id"]))
-
-                match, options = check_compatibility(s1, s2)
-                if not match:
-                    logging.debug("No match.")
-                    continue
-
-                new_url = str(uuid4()).replace("-", "")
-                logging.info(
-                    "Matched %s and %s, sending to %s."
-                    % (s1["id"], s2["id"], new_url)
-                )
-                new_chat = SearchedChat(url=new_url)
-                db.add(new_chat)
-                db.flush()
-
-                db.add(ChatUser(chat_id=new_chat.id, user_id=s1["user_id"], number=1, **s1["character"]))
-                db.add(ChatUser(chat_id=new_chat.id, user_id=s2["user_id"], number=2, **s2["character"]))
-
-                if options:
-                    db.add(Message(
-                        chat_id=new_chat.id,
-                        type="search_info",
-                        text=" ".join(option_messages[_] for _ in options),
-                    ))
-
-                db.commit()
-
-                already_matched.add(s1["id"])
-                already_matched.add(s2["id"])
-
-                match_message = json.dumps({ "status": "matched", "url": new_url })
-                redis.publish("searcher:%s" % s1["id"], match_message)
-                redis.publish("searcher:%s" % s2["id"], match_message)
-                searcher_ids.remove(s1["id"])
-                searcher_ids.remove(s2["id"])
+    run_matchmaker(get_searcher_info, check_compatibility, SearchedChat, get_character_info)
 
