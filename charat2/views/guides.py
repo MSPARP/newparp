096b9783dc5b8cb0f2a20e9d3d57d3897d36b1ff
Olly Parton <mysticdragonfly@hotmail.co.uk>
2015-10-16 19:04:56 +0100

    Set response encoding based on apparent_encoding.
    Headers from drweeaboo.net incorrectly tell us ISO-8859-1.

--- charat2/views/guides.py
+++ charat2/views/guides.py
@@ -2,5 +2,6 @@ import requests
 
 def user_guide():
     r = requests.get("http://drweeaboo.net/msparp/userguide/duplicateguide.html")
+    r.encoding = r.apparent_encoding
     return r.text, r.status_code
 
