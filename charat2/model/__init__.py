78b54f451f2f602f7b5edc694438b92ff343b272
thecount92 <ddracul@dlh-digital.com>
2016-01-07 04:56:47 +0000

    Update __init__.py
    trying to fix the thing...

--- charat2/model/__init__.py
+++ charat2/model/__init__.py
@@ -39,8 +39,7 @@ engine = create_engine(
     echo="ECHO" in os.environ or "--debug" in sys.argv,
     pool_size=100,
     max_overflow=200,
-    pool_timeout=30,
-    pool_recycle=3600,
+    pool_recycle=1800,
 )
 
 sm = sessionmaker(
