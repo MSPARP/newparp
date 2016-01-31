3e86072667d486cb75e0cefca847bbdd2f032023
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-01-31 23:04:11 +0000

    Update the user guide URL.

--- charat2/views/guides.py
+++ charat2/views/guides.py
@@ -1,7 +1,7 @@
 import requests
 
 def user_guide():
-    r = requests.get("http://drweeaboo.net/msparp/userguide/duplicateguide.html")
+    r = requests.get("https://karry.terminallycapricio.us/userguide/duplicateguide.html")
     r.encoding = r.apparent_encoding
     return r.text, r.status_code
 
