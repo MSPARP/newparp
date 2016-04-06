52d55332335a71ec1704e8501dd3fc60a403705b
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-04-06 22:19:04 +0100

    Use ISO 8601 for times.

--- newparp/model/__init__.py
+++ newparp/model/__init__.py
@@ -190,8 +190,8 @@ class User(Base):
             "username": self.username,
             "group": self.group,
             "is_admin": self.is_admin,
-            "created": time.mktime(self.created.timetuple()),
-            "last_online": time.mktime(self.last_online.timetuple()),
+            "created": self.created.isoformat(),
+            "last_online": self.last_online.isoformat(),
             "name": self.name,
             "acronym": self.acronym,
             "color": self.color,
@@ -226,7 +226,7 @@ class Block(Base):
     def to_dict(self, include_users=False):
         bd = {
             "chat": self.chat.to_dict(),
-            "created": time.mktime(self.created.timetuple()),
+            "created": self.created.isoformat(),
             "reason": self.reason,
         }
         if include_users:
@@ -797,7 +797,7 @@ class Message(Base):
         md = {
             "id": self.id,
             "user_number": self.chat_user.number if self.chat_user is not None else None,
-            "posted": time.mktime(self.posted.timetuple()),
+            "posted": self.posted.isoformat(),
             "type": self.type,
             "color": self.color,
             "acronym": self.acronym,
@@ -905,7 +905,7 @@ class AdminLogEntry(Base):
     def to_dict(self):
         return {
             "id": self.id,
-            "date": time.mktime(self.date.timetuple()),
+            "date": self.date.isoformat(),
             "action_user": self.action_user.to_dict(),
             "type": self.type,
             "description": self.description,
@@ -928,7 +928,7 @@ class IPBan(Base):
     def to_dict(self):
         return {
             "address": self.address,
-            "date": time.mktime(self.date.timetuple()),
+            "date": self.date.isoformat(),
             "creator": self.creator.to_dict(),
             "reason": self.reason,
         }
