9590f587cb806a8e527b2243e66bc976e77b5e91
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-06-19 22:59:59 +0100

    Add some randomness to the socket reconnection code.

--- newparp/static/js/newparp.js
+++ newparp/static/js/newparp.js
@@ -659,7 +659,7 @@ var msparp = (function() {
 						// Otherwise try to reconnect.
 						exit();
 						status_bar.css("color", "#f00").text("Sorry, the connection to the server has been lost. Attempting to reconnect...");
-						window.setTimeout(launch_websocket, 2000);
+						window.setTimeout(launch_websocket, Math.random() * 10000);
 					}
 				}
 			}
