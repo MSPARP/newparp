40f7f0b44d819efd4f4a1ccb84df7baf0470532d
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-12-17 23:35:20 +0000

    Check email bans when activating.

--- newparp/views/settings.py
+++ newparp/views/settings.py
@@ -1,12 +1,12 @@
 from flask import abort, current_app, g, jsonify, redirect, render_template, request, url_for
-from sqlalchemy import and_
+from sqlalchemy import and_, func, literal
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 
 from newparp.helpers import alt_formats, themes
 from newparp.helpers.auth import log_in_required
 from newparp.helpers.email import send_email
-from newparp.model import Block, User
+from newparp.model import Block, EmailBan, User
 from newparp.model.connections import use_db
 from newparp.model.validators import email_validator
 
@@ -132,10 +132,21 @@ def verify_email():
 
     user.email_address = email_address
     user.email_verified = True
-    if user.group == "new":
-        user.group = "active"
 
     g.redis.set("session:" + g.session_id, user.id, 2592000)
+
+    if user.group == "new":
+        email_bans = (
+            g.db.query(func.count("*"))
+            .select_from(EmailBan)
+            .filter(literal(user.email_address).op("~*")(EmailBan.pattern))
+            .scalar()
+        )
+        if email_bans:
+            user.group = "deactivated"
+        else:
+            user.group = "active"
+
     return redirect(url_for("settings_log_in_details", saved=next_message))
 
 
