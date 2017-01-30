ff38b3b75141c0eac3aa9d0874def4c0d54d45c4
nepeat <nepeat@users.noreply.github.com>
2017-01-30 02:15:51 -0900

    Move redirect test to a separate file.

--- tests/web/test_groups.py
+++ tests/web/test_groups.py
@@ -62,20 +62,4 @@
 #             })
 #             assert rv.status_code == 403
 #
-#     def test_redirect(self):
-#         INVALID_URL = "http://www.mspaintadventures.com/ACT6ACT6.php?s=6&p=009309"
-#         urls = {
-#             "fake": INVALID_URL,
-#             "http://www.mspaintadventures.com/ACT6ACT6.php?s=6&p=009309": INVALID_URL,
-#             "https://google.com": "https://google.com",
-#             "http://example.com": "http://example.com",
-#             "/theoubliette/users/unban": INVALID_URL,
-#             "ftp://127.0.0.1/": INVALID_URL
-#         }
-#
-#         for test_url, expected_url in urls.items():
-#             test_encoded = urllib.parse.urlencode({"url": test_url})
-#             rv = self.flask_client.get("/redirect?" + test_encoded)
-#
-#             assert rv.status_code == 200
-#             assert html.escape(expected_url).encode("utf8") in rv.data
+
